6a7e8e79abe576031c23b2596736531542003218
Abraham Angene <124372191+abe-maker@users.noreply.github.com>
2026-01-23 19:25:11 +0100

    xxx

--- quiz.py
+++ quiz.py
@@ -1,5 +1,7 @@
 questions = [
-    {"prompt": "Which city is the Capital of France?", "options": "A:Bourdeaux, B:Cannes, C:Paris, D:Marssailes", "answer": "C"}, {"prompt": "Whats the best country of africa?","options": "A:Nigeria, B:Mozambique, C:Uganda, D:South Africa", "answer":"D"}, {"prompt": "Whats the coolest motorcycle?", "options": "A: Royal Enfield Himalayan 411, B: Triumph Bonneville 900, C: Yamaha MT-09, D: Honda Rebel 750", "answer": "B"}
+    {"prompt": "Which city is the Capital of France?", "options": "A:Bourdeaux, B:Cannes, C:Paris, D:Marssailes", "answer": "C"}, 
+    {"prompt": "Whats the best country of africa?","options": "A:Nigeria, B:Mozambique, C:Uganda, D:South Africa", "answer":"D"}, 
+    {"prompt": "Whats the coolest motorcycle?", "options": "A: Royal Enfield Himalayan 411, B: Triumph Bonneville 900, C: Yamaha MT-09, D: Honda Rebel 750", "answer": "B"}
 ]
 
 #print question and userinput
