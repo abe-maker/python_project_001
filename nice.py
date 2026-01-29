52498189e7bf8ef2cb2f69744a559984ddc0c99c
Abraham Angene <124372191+abe-maker@users.noreply.github.com>
2026-01-29 13:23:00 +0100

    update

--- nice.py
+++ nice.py
@@ -1,5 +1,29 @@
 from nicegui import ui
 
-ui.label('Hello NiceGUI!')
+ui.label('CALCULATOR').classes("bold item-center")
+
+
+with ui.grid(columns = 4):
+    ui.button("1")
+    ui.button("2")
+    ui.button("3")
+    ui.button("/")
+
+    ui.button("4")
+    ui.button("5")
+    ui.button("6")
+    ui.button("*")
+
+    ui.button("7")
+    ui.button("8")
+    ui.button("9")
+    ui.button("-")
+
+    ui.button("")
+    ui.button("0")
+    ui.button("")
+    ui.button("+")
+
+ui.button("calculate")
 
 ui.run()
