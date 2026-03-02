11d8db7ece6a8893ce39040ea2b7c123336c7bee
Abraham Angene <124372191+abe-maker@users.noreply.github.com>
2026-03-02 12:15:58 +0100

    update

---
+++ test.py
@@ -0,0 +1,13 @@
+new_set = {1, 2, 3, 4, 5}
+print(new_set)
+new_set.add(6)
+print(new_set)
+new_set.remove(3)
+print(new_set)
+new_set.clear()
+print(new_set)
+
+new_dictionary = {"name": "John", "age": 30, "city": "New York"}
+print(new_dictionary)
+print(new_dictionary["name"])
+print(new_dictionary["age"])
