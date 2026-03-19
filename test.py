083b4ca107cfcbe223f16e1c346b1ae62d3dcf3a
Abraham Angene <124372191+abe-maker@users.noreply.github.com>
2026-03-19 12:28:32 +0100

    update

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-new_set = {1, 2, 3, 4, 5}
-print(new_set)
-new_set.add(6)
-print(new_set)
-new_set.remove(3)
-print(new_set)
-new_set.clear()
-print(new_set)
+import math
+import logging
 
-new_dictionary = {"name": "John", "age": 30, "city": "New York"}
-print(new_dictionary)
-print(new_dictionary["name"])
-print(new_dictionary["age"])
+logging.basicConfig(level = logging.DEBUG, filename = "mylog.log", filemode = "w", format = "%(asctime)s")
+
+
+print(var)
+
+var = 5
